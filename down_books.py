784cbd6ae0eb900da5f2a103c5f39844ce46c023
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-29 14:01:39 +0300

    Скачивает только книги, без HTTP кодов

--- down_books.py
+++ down_books.py
@@ -3,10 +3,17 @@ import os
 import requests
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
 def download_book(id, number):
     url = f'https://tululu.org/txt.php?id={id}'  # 32168
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=True)
     response.raise_for_status()
+
+    check_for_redirect(response)
 
     if not os.path.exists('books'):
         os.makedirs('books')
@@ -14,7 +21,14 @@ def download_book(id, number):
         ff.write(response.content)
 
 
-if __name__ == '__main__':
+def main():
     for number_book in range(1, 11):
         id_book = 32168 + number_book
-        download_book(id_book, number_book)
+        try:
+            download_book(id_book, number_book)
+        except requests.exceptions.HTTPError:
+            continue
+
+
+if __name__ == '__main__':
+    main()
