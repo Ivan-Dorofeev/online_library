e722d575f27c0b4456d371cc0b39c1321101f9e3
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-29 11:44:14 +0300

    download books from tululu.org.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import os
+
 import requests
 
 
-def download_book():
-    url = 'https://tululu.org/txt.php?id=32168'
+def download_book(id, number):
+    url = f'https://tululu.org/txt.php?id={id}'  # 32168
     response = requests.get(url)
     response.raise_for_status()
-    with open('Пески_Марса.txt', 'wb') as ff:
+
+    if not os.path.exists('books'):
+        os.makedirs('books')
+    with open(f'books/{number}.txt', 'wb') as ff:
         ff.write(response.content)
 
 
 if __name__ == '__main__':
-    download_book()
+    for number_book in range(1, 11):
+        id_book = 32168 + number_book
+        download_book(id_book, number_book)
