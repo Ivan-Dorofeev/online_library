6d40c77960e31df1ba95fecc0fee4db0874c13bf
Ivan Dorofeev <axxel123@rambler.ru>
2022-09-06 08:48:04 +0300

    Парсим 10 страниц

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,35 +4,40 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_book_ids():
+def get_max_pages(url):
+    response = requests.get(url, allow_redirects=True)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    pages = soup.find_all('a', class_='npage')[-1].text
+    return int(pages)
+
+
+def get_book_links(page):
     fantastic_books_url = 'https://tululu.org/l55/'
     response = requests.get(fantastic_books_url, allow_redirects=True)
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
-    books_link = soup.find('td', class_='ow_px_td').find_all('a')
+    books_ids = soup.find('td', class_='ow_px_td').find_all('a')
 
-    book_ids = []
-    for book_link in books_link:
-        if str(book_link['href']).startswith('/b'):
-            if book_link['href'] not in book_ids:
-                book_ids.append(book_link['href'])
-    return book_ids
-
-
-def get_book_lonks(book_ids):
     book_urls = []
-    for book_id in book_ids:
-        url = urljoin('https://tululu.org', book_id)
-        book_urls.append(url)
-    print(book_urls)
+    for books_id in books_ids:
+        if str(books_id['href']).startswith('/b'):
+            url = urljoin('https://tululu.org', books_id['href'])
+            if url not in book_urls:
+                book_urls.append(url)
     return book_urls
 
 
 def main():
-    book_ids = get_book_ids()
-    book_links = get_book_lonks(book_ids)
-    print(book_ids)
+    book_urls = []
+    # max_pages = get_max_pages('https://tululu.org/l55/')
+
+    for page in range(1, 11):
+        book_urls += get_book_links(page)
+        print(len(book_urls))
+    print(book_urls)
 
 
 if __name__ == '__main__':
