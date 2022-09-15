de62cbd352bc8153a88b327217ebd17dff6a58e3
Ivan Dorofeev <axxel123@rambler.ru>
2022-09-15 08:23:56 +0300

    Correct variavle's names

--- render_website.py
+++ render_website.py
@@ -22,11 +22,11 @@ def rebuild():
 
     books_on_page = 10
     books_part = list(chunked(json_books, books_on_page))
-    pages_count = list(range(1, len(books_part) + 1))
+    pages_numbers = list(range(1, len(books_part) + 1))
 
     for page_number, ten_books in enumerate(books_part, 1):
         books_in_row = list(chunked(ten_books, 2))
-        rendered_page = template.render(books=books_in_row, pages=pages_count, current_page=page_number,
+        rendered_page = template.render(books=books_in_row, pages=pages_numbers, current_page=page_number,
                                         last_page=len(books_part))
         path = os.path.join('pages', f'index{page_number}.html')
         with open(path, 'w', encoding="utf8") as file:
